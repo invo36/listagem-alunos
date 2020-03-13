b490ddae124dceb7d9ee226bdd80182831badb8d
Kevin Araujo de Oliveira <kevinaraujoo677@gmail.com>
2020-03-13 20:51:26 -0300

    Adicionando Bootstrap e mexendo no design das duas telas

--- index.php
+++ index.php
@@ -3,114 +3,197 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        body{
-            margin: 0;
-            padding: 0; 
-            background: #3C5680;
-        }
-        label{
-            margin-bottom: 5px;
-        }
-        span{
-            display: block;
-            margin: 10px 0 10px 0;
-            font-weight: 1000;
-            font-size: 17px;
-        }
-        h2{
-            text-align: center;
-        }
-        .todo{
-            background: #E8EFFF;
-            width: 90vh;
-            display: block;
-            margin: auto;
-            margin-top: 30px;
-            border-radius: 15px;
-            padding: 20px;
-        }
-        .separa{
-            margin-bottom: 15px;
-            width: 100%;
-            display: flex;
-        }
-        .separa2{
-            width: 50%;
-        }
-        .flexbox{
-            display: flex;
-        }
-    </style>
-
+    <title>Diário do Professor</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body>
+<body class="bg-primary">
+    <div class="container bg-white pb-1">
+        <h2 class="display-3 text-center pb-4">Diário de Presença</h2>
 
-    <div class="todo">
-        <h2>Diário de Presença</h2>
-        <form action="result.php" method="POST">
-        <input type="date" required name="data">
-            <div class="flexbox">
-                <div class="separa2">Aluno</div>
-                <div class="separa2">Frequência</div>
+        <!-- Formulário -->
+
+        <form action="result.php" method="post">
+            <div class="form-group text-center">
+                <p class="h5">Dia da aula</p>
+                <input type="date" class="form-control" required name="data" style="width: 50%; margin: auto;">
             </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome1" value="BOB"> 
-                    <div class='separa2'>
-                        <label>Presente:<input type="radio" name="pres1" checked value="Presente"></label>
-                        <label>Falta:<input type="radio" name="pres1" value="Falta"></label>
+            <hr>
+            <!-- Parte de colocar presença e falta -->
+
+            <div class="row ml-5">
+                <div class="col-sm-7">
+                    <p class="h5">Aluno</p>
+
+                    <!-- Alunos 1 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome1" class="form-control" value="BOB">
+                    </div>
+
+                    <!-- Aluno 2 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome2" class="form-control" value="Corona">
+                    </div>
+
+                    <!-- Aluno 3 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome3" class="form-control" value="Lucas">
+                    </div>
+
+                    <!-- Aluno 4 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome4" class="form-control" value="ADM">
+                    </div>
+
+                    <!-- Aluno 5 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome5" class="form-control" value="Kevin">
+                    </div>
+
+                    <!-- Aluno 6 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome6" class="form-control" value="Walace">
+                    </div>
+
+                    <!-- Aluno 7 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome7" class="form-control" value="Mão na vara">
+                    </div>
+
+                    <!-- Aluno 8 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome8" class="form-control" value="Douglas">
+                    </div>
+
+                    <!-- Aluno 9 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome9" class="form-control" value="Freitas e Nuguet(amor eterno)">
+                    </div>
+
+                    <!-- Aluno 10 -->
+                    <div class="form-group">
+                        <input type="text" readonly name="nome10" class="form-control" value="Felipe Neto">
                     </div>
                 </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome2"  value="Corona"> 
-                    <label>Presente:<input type="radio" name="pres2" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres2" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome3" value="Lucas">    
-                    <label>Presente:<input type="radio" name="pres3" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres3" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome4" value="ADM">    
-                    <label>Presente:<input type="radio" name="pres4" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres4" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome5" value="Kevin">    
-                    <label>Presente:<input type="radio" name="pres5" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres5" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome6" value="Walace">    
-                    <label>Presente:<input type="radio" name="pres6" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres6" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome7" value="Mão na vara">    
-                    <label>Presente:<input type="radio" name="pres7" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres7" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome8" value="Douglas">    
-                    <label>Presente:<input type="radio" name="pres8" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres8" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome9" value="Freitas e Nuguet(amor eterno)">    
-                    <label>Presente:<input type="radio" name="pres9" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres9" value="Falta"></label>
-                </div>
-                <div class="separa">
-                    <input type="text" readonly name="nome10" value="Felipe Neto">    
-                    <label>Presente:<input type="radio" name="pres10" checked value="Presente"></label>
-                    <label>Falta:<input type="radio" name="pres10" value="Falta"></label>
-                </div>
+                <div class="col-5">
+                    <p class="h5">Frequência</p>
+                    <!-- Frequência de aluno 2 -->
+                    <div class="form-group pt-1">
+                        <div class="form-check">
+                            <label class="form-check-label" for="checka1">Presente:</label>
+                            <input type="radio" id="checka1" name="pres1" checked value="Presente">
 
-            <input type="submit" value="Enviar">
-    </form>
+                            <label class="form-check-label" for="checkb1">Falta:</label>
+                            <input type="radio" name="pres1" id="checkb1" value="Falta">
+                        </div>
+                    </div>
+                    
+                    <!-- Frequência de aluno 2 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka2">Presente:</label>
+                            <input type="radio" id="checka2" name="pres2" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb2">Falta:</label>
+                            <input type="radio" name="pres2" id="checkb2" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 3 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka3">Presente:</label>
+                            <input type="radio" id="checka3" name="pres3" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb3">Falta:</label>
+                            <input type="radio" name="pres3" id="checkb3" value="Falta">
+                        </div>
+                    </div>
+                    
+                    <!-- Frequência de aluno 4 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka4">Presente:</label>
+                            <input type="radio" id="checka4" name="pres4" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb4">Falta:</label>
+                            <input type="radio" name="pres4" id="checkb4" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 5 -->
+                    <div class="form-group">
+                        <div class="form-check pt-2">
+                            <label class="form-check-label" for="checka5">Presente:</label>
+                            <input type="radio" id="checka5" name="pres5" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb5">Falta:</label>
+                            <input type="radio" name="pres5" id="checkb5" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 6 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka6">Presente:</label>
+                            <input type="radio" id="checka6" name="pres6" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb6">Falta:</label>
+                            <input type="radio" name="pres6" id="checkb6" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 7 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka7">Presente:</label>
+                            <input type="radio" id="checka7" name="pres7" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb7">Falta:</label>
+                            <input type="radio" name="pres7" id="checkb7" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 8 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka8">Presente:</label>
+                            <input type="radio" id="checka8" name="pres8" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb8">Falta:</label>
+                            <input type="radio" name="pres8" id="checkb8" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 9 -->
+                    <div class="form-group">
+                        <div class="form-check pt-3">
+                            <label class="form-check-label" for="checka9">Presente:</label>
+                            <input type="radio" id="checka9" name="pres9" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb9">Falta:</label>
+                            <input type="radio" name="pres9" id="checkb9" value="Falta">
+                        </div>
+                    </div>
+
+                    <!-- Frequência de aluno 10 -->
+                    <div class="form-group">
+                        <div class="form-check pt-2">
+                            <label class="form-check-label" for="checka10">Presente:</label>
+                            <input type="radio" id="checka10" name="pres10" checked value="Presente">
+
+                            <label class="form-check-label" for="checkb10">Falta:</label>
+                            <input type="radio" name="pres10" id="checkb10" value="Falta">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <input type="submit" class="btn btn-primary mb-4" value="Enviar" style="display:block; margin: 0 auto;">
+        </form>
+
     </div>
+
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
--- result.php
+++ result.php
@@ -1,64 +1,45 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <style>
-        body{
-            margin: 0;
-            padding: 0; 
-            background: #3C5680;
-        }
-        label{
-            display: block;
-            margin-bottom: 5px;
-        }
-        span{
-            display: block;
-            margin: 10px 0 10px 0;
-            font-weight: 1000;
-            font-size: 17px;
-        }
-        h2{
-            text-align: center;
-        }
-        div{
-            background: #E8EFFF;
-            width: 90vh;
-            display: block;
-            margin: auto;
-            margin-top: 30px;
-            border-radius: 15px;
-            padding: 20px;
-        }
-    </style>
+    <title>Diário do Professor</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body>
-    <div>
-        <h2>Relatório de Presença</h2>
-        <table border='1'>
-            <thead>
-                <td>ALUNO
-                <td>FREQUÊNCIA
+<body class="bg-primary">
+    <div class="container bg-white pb-1">
+        <h2 class="display-3 text-center pb-4">Relatório de Presença</h2>
+
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Aluno
+                    <th scope="col" class="text-center">Frequência
             </thead>
-        <?php 
+        
+        
+        <?php
             $data = $_POST['data'];
             $data2 = preg_split('/-/', $data);
             $data2 = array_reverse($data2);
-            echo $data2[0]. '/'. $data2[1]. '/'. $data2[2];
+            echo "<div class='h6 text-center'>Essa aula foi ministrada no dia: ".$data2[0]. '/'. $data2[1]. '/'. $data2[2]."</div>";
+
             $nome = array();
             $pres = array();
         for($i = 0; $i < 10; $i++){
             array_push($nome, $_POST['nome'. ($i + 1)]);
             array_push($pres, $_POST['pres'. ($i + 1)]);
-            echo '<td>';
-            echo $nome[$i]. '<td>' .$pres[$i]. '<tr>';
+            echo '<tbody ><td>';
+            echo $nome[$i]. '<td class="text-center">' .$pres[$i]. '<tr></tbody>';
         }
         ?>
-    </table>
-</div>
+        </table>
+        <a href="index.php"><input type="button" class="btn btn-primary" value="Voltar"></a>
+    </div>
+
+
+    <script src="js/jquery-3.4.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
